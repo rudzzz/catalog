aabd9ceecd222c102c35e32cb6a5b8a813c9fad2
rudzzz <rudsolza13@gmail.com>
2022-11-04 00:03:37 -0300

    code

--- backend/src/main/java/com/dev/catalog/services/CategoryService.java
+++ backend/src/main/java/com/dev/catalog/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.dev.catalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.dev.catalog.dto.CategoryDTO;
 import com.dev.catalog.entities.Category;
 import com.dev.catalog.repositories.CategoryRepository;
 
@@ -16,7 +18,8 @@ public class CategoryService {
 	private CategoryRepository categoryRepository;
 	
 	@Transactional(readOnly=true)
-	public List<Category> findall(){
-		return categoryRepository.findAll();
+	public List<CategoryDTO> findall(){
+		List<Category> list = categoryRepository.findAll();
+		return list.stream().map(listElement -> new CategoryDTO(listElement)).collect(Collectors.toList());
 	}
 }
