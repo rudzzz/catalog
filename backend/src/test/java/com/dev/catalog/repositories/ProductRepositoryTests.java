c6b96687e77f1c978917964457d23932c2781684
rudzzz <rudsolza13@gmail.com>
2022-12-28 20:13:06 -0300

    code

--- backend/src/test/java/com/dev/catalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/dev/catalog/repositories/ProductRepositoryTests.java
@@ -2,6 +2,7 @@ package com.dev.catalog.repositories;
 
 import java.util.Optional;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -19,6 +20,8 @@ public class ProductRepositoryTests {
 		long id = 1L;
 		productRepositoy.deleteById(id);
 		
-		Optional<Product> result = productRepositoy.findBy(id);
+		Optional<Product> result = productRepositoy.findById(id);
+		
+		Assertions.assertFalse(result.isPresent());
 	}
 }
