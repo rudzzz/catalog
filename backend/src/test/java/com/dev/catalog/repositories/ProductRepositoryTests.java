dcebd2dff6ebf5dfcbadc27504f4d5692fd4b21b
rudzzz <rudsolza13@gmail.com>
2022-12-28 20:10:04 -0300

    code

--- backend/src/test/java/com/dev/catalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/dev/catalog/repositories/ProductRepositoryTests.java
@@ -1,15 +1,24 @@
 package com.dev.catalog.repositories;
 
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import com.dev.catalog.entities.Product;
+import com.dev.catalog.repositories.ProductRepository;
+
 @DataJpaTest
 public class ProductRepositoryTests {
 	@Autowired 
-	private ProductRepositoy productRepositoy;
+	private ProductRepository productRepositoy;
+	
 	@Test
 	public void deleteShouldDeleteObjectWhenIdExistis() {
+		long id = 1L;
+		productRepositoy.deleteById(id);
 		
+		Optional<Product> result = productRepositoy.findBy(id);
 	}
 }
