7f0e0a26ab32d1e9121b6626714add07277751b7
rudzzz <rudsolza13@gmail.com>
2022-12-29 15:14:38 -0300

    code

--- backend/src/test/java/com/dev/catalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/dev/catalog/repositories/ProductRepositoryTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import com.dev.catalog.entities.Product;
 import com.dev.catalog.repositories.ProductRepository;
@@ -23,5 +24,13 @@ public class ProductRepositoryTests {
 		Optional<Product> result = productRepositoy.findById(id);
 		
 		Assertions.assertFalse(result.isPresent());
+	}
+	
+	@Test
+	public void deleteShouldThrowEmptyResultDataAccessExceptionWhenIdDoesNotExist() {
+		Assertions.assertThrows(EmptyResultDataAccessException.class, () -> {
+			long nonExistingId = 1000L;
+			productRepositoy.deleteById(nonExistingId);
+		});
 	}
 }
